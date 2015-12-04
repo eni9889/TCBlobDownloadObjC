3cbddb62c5c28433b9cbe1547d55248be4ff6d76
Enea Gjoka <enea@unlimapps.com>
2015-12-04 11:55:34 -0800

    Started converting the download manager to ObjC

--- Pod/Classes/TCBlobDownloadManager.h
+++ Pod/Classes/TCBlobDownloadManager.h
@@ -7,7 +7,69 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "TCBlobDownload.h"
+
+const NSString *kTCBlobDownloadSessionIdentifier = @"tcblobdownloadmanager_downloads";
+const NSString *kTCBlobDownloadErrorDomain = @"com.tcblobdownloadswift.error";
+const NSString *kTCBlobDownloadErrorDescriptionKey = @"TCBlobDownloadErrorDescriptionKey";
+const NSString *kTCBlobDownloadErrorHTTPStatusKey = @"TCBlobDownloadErrorHTTPStatusKey";
+const NSString *kTCBlobDownloadErrorFailingURLKey = @"TCBlobDownloadFailingURLKey";
+
+typedef enum {
+    TCBlobDownloadHTTPError = 1
+} TCBlobDownloadError;
+
+@interface DownloadDelegate : NSObject <NSURLSessionDownloadDelegate>
+@property (nonatomic, strong) NSMutableArray *downloads;
+@end
 
 @interface TCBlobDownloadManager : NSObject
 
+@property (nonatomic, strong) DownloadDelegate *delegate;
+@property (nonatomic, strong) NSURLSession *session;
+
+@property (nonatomic, assign) BOOL startImmediatly;
+
+/**
+ Custom `NSURLSessionConfiguration` init.
+ :param: config The configuration used to manage the underlying session.
+ */
+-(instancetype)initWithConfig:(NSURLSessionConfiguration *)config;
+
+/**
+ Start downloading the file at the given URL.
+ 
+ :param: url NSURL of the file to download.
+ :param: directory Directory Where to copy the file once the download is completed. If `nil`, the file will be downloaded in the current user temporary directory/
+ :param: name Name to give to the file once the download is completed.
+ :param: delegate An eventual delegate for this download.
+ :return: A `TCBlobDownload` instance.
+ */
+
+-(TCBlobDownload *)downloadFileAtURL:(NSURL *)url toDirectory:(NSURL *)directory withName:(NSString *)name andDelegate:(id <TCBlobDownloadDelegate>)delegate;
+
+/**
+ Start downloading the file at the given URL.
+ :param: url NSURL of the file to download.
+ :param: directory Directory Where to copy the file once the download is completed. If `nil`, the file will be downloaded in the current user temporary directory/
+ :param: name Name to give to the file once the download is completed.
+ :param: progression A closure executed periodically when a chunk of data is received.
+ :param: completion A closure executed when the download has been completed.
+ :return: A `TCBlobDownload` instance.
+ */
+
+-(TCBlobDownload *)downloadFileAtURL:(NSURL *)url toDirectory:(NSURL *)directory withName:(NSString *)name progress:(TCBlobDownloadProgressHandler)progressHandler completion:(TCBlobDownloadCompletionHandler)completionHandler;
+
+/**
+ Resume a download with previously acquired resume data.
+ 
+ :see: `TCBlobDownload -cancelWithResumeData:` to produce this data.
+ :param: resumeData Data blob produced by a previous download cancellation.
+ :param: directory Directory Where to copy the file once the download is completed. If `nil`, the file will be downloaded in the current user temporary directory/
+ :param: name Name to give to the file once the download is completed.
+ :param: delegate An eventual delegate for this download.
+ 
+ :return: A `TCBlobDownload` instance.
+ */
+-(TCBlobDownload *)downloadFileWithResumeData:(NSData *)resumeData toDirectory:(NSURL *)directory withName:(NSString *)name andDelegate:(id <TCBlobDownloadDelegate>)delegate;
 @end
--- Pod/Classes/TCBlobDownloadManager.m
+++ Pod/Classes/TCBlobDownloadManager.m
@@ -10,4 +10,48 @@
 
 @implementation TCBlobDownloadManager
 
+-(instancetype)init {
+    return [self initWithConfig:[NSURLSessionConfiguration defaultSessionConfiguration]];
+}
+
+-(instancetype)initWithConfig:(NSURLSessionConfiguration *)config {
+    if (self = [super init]) {
+        self.delegate = [[DownloadDelegate alloc] init];
+        self.session = [NSURLSession sessionWithConfiguration:config delegate:self.delegate delegateQueue:nil];
+        self.session.sessionDescription = @"TCBlobDownloadManger session";
+    }
+    return self;
+}
+
+-(TCBlobDownload *)downloadFileAtURL:(NSURL *)url toDirectory:(NSURL *)directory withName:(NSString *)name andDelegate:(id <TCBlobDownloadDelegate>)delegate {
+    NSURLSessionDownloadTask *downloadTask = [self.session downloadTaskWithURL:url];
+    TCBlobDownload *download = [[TCBlobDownload alloc] initWithTask:downloadTask toDirectory:directory fileName:name delegate:delegate];
+    return [self downloadWithDownload:download];
+}
+
+-(TCBlobDownload *)downloadFileAtURL:(NSURL *)url toDirectory:(NSURL *)directory withName:(NSString *)name progress:(TCBlobDownloadProgressHandler)progressHandler completion:(TCBlobDownloadCompletionHandler)completionHandler {
+    NSURLSessionDownloadTask *downloadTask = [self.session downloadTaskWithURL:url];
+    TCBlobDownload *download = [[TCBlobDownload alloc] initWithTask:downloadTask toDirectory:directory fileName:name progress:progressHandler completion:completionHandler];
+    return [self downloadWithDownload:download];
+}
+
+-(TCBlobDownload *)downloadFileWithResumeData:(NSData *)resumeData toDirectory:(NSURL *)directory withName:(NSString *)name andDelegate:(id <TCBlobDownloadDelegate>)delegate {
+    NSURLSessionDownloadTask *downloadTask = [self.session downloadTaskWithResumeData:resumeData];
+    TCBlobDownload *download = [[TCBlobDownload alloc] initWithTask:downloadTask toDirectory:directory fileName:name delegate:delegate];
+    return [self downloadWithDownload:download];
+}
+
+/**
+ Base method to start a download, called by other download methods.
+ 
+ :param: download Download to start.
+ */
+-(TCBlobDownload *)downloadWithDownload:(TCBlobDownload *)download {
+    self.delegate.downloads[download.downloadTask.taskIdentifier] = download;
+    if (self.startImmediatly) {
+        [download.downloadTask resume];
+    }
+    return download;
+}
+
 @end
